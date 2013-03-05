9c12a32db7d07187b7096b33e5d9ddbc1c21b907
seb <seb@cygnus.(none)>
2013-03-05 07:28:33 +0100

    Make the clone div hidden, so that it never shows.

--- src/areacomplete.js
+++ src/areacomplete.js
@@ -236,7 +236,8 @@
 			"overflow-x" : "hidden",
 			"line-height" :  $(data.ta).css("line-height"),
 			"overflow-y" : "hidden",
-			"z-index" : -10
+			"z-index" : -10,
+      "visibility": "hidden"
 		});
 		
 		//console.log("createClone: ta width=",$(data.ta).css("width")," ta clientWidth=",data.ta.clientWidth, "scrollWidth=",data.ta.scrollWidth," offsetWidth=",data.ta.offsetWidth," jquery.width=",$(data.ta).width());
